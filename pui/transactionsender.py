a8aca69430ca1d60a92afb0663dedae276952bf5
Payroma Developer <payromafinance@gmail.com>
2022-03-09 17:01:32 +0200

    Update transaction sender interface

--- pui/transactionsender.py
+++ pui/transactionsender.py
@@ -4,30 +4,112 @@ from pcontroller import translator
 from pui import SetupForm, fonts, images, styles, Size, qlabeladdress, assetsicons
 
 
+class NetworkWidget(QWidget):
+    def __init__(self, parent):
+        super(NetworkWidget, self).__init__(parent, flags=Qt.SubWindow)
+
+        self.setLayout(QGridLayout())
+        self.layout().setAlignment(Qt.AlignLeft)
+        self.layout().setContentsMargins(0, 0, 0, 0)
+        self.layout().setHorizontalSpacing(11)
+        self.layout().setVerticalSpacing(0)
+
+        self.labelIcon = SPGraphics.QuickLabel(
+            self, fixed_size=Size.s41, pixmap=images.data.icons.info41, align=Qt.AlignCenter
+        )
+
+        self.labelTitle = SPGraphics.QuickLabel(
+            self, fixed_height=21
+        )
+        self.labelTitle.setWordWrap(False)
+
+        self.pushButton = SPGraphics.QuickPushButton(
+            fixed_height=21, value_changed=QObject.mainModel.textColorAnimated,
+            start_value=styles.data.colors.font_description, end_value=styles.data.colors.highlight
+        )
+        self.pushButton.setSizePolicy(QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed))
+
+        self.layout().addWidget(self.labelIcon, 0, 0, 2, 1)
+        self.layout().addWidget(self.labelTitle, 0, 1, 1, 1)
+        self.layout().addWidget(self.pushButton, 1, 1, 1, 1)
+
+
+class GasWidget(QWidget):
+    def __init__(self, parent):
+        super(GasWidget, self).__init__(parent, flags=Qt.SubWindow)
+
+        self.setAttribute(Qt.WA_StyledBackground, True)
+        self.setLayout(QGridLayout())
+        self.layout().setContentsMargins(21, 21, 21, 21)
+        self.layout().setSpacing(11)
+
+        self.labelEstimatedFee = SPGraphics.QuickLabel(
+            self, fixed_height=21
+        )
+        self.labelEstimatedFee.setWordWrap(False)
+
+        self.labelEstimatedFeeValue = SPGraphics.QuickLabel(
+            self, fixed_height=21, align=Qt.AlignRight
+        )
+        self.labelEstimatedFeeValue.setWordWrap(False)
+
+        self.labelMaxFee = SPGraphics.QuickLabel(
+            self, fixed_height=21
+        )
+        self.labelMaxFee.setWordWrap(False)
+        self.labelMaxFee.setObjectName('labelDescription')
+
+        self.labelMaxFeeValue = SPGraphics.QuickLabel(
+            self, fixed_height=21, align=Qt.AlignRight
+        )
+        self.labelMaxFeeValue.setWordWrap(False)
+        self.labelMaxFeeValue.setObjectName('labelDescription')
+
+        self.labelTotal = SPGraphics.QuickLabel(
+            self, fixed_height=21
+        )
+        self.labelTotal.setWordWrap(False)
+
+        self.labelTotalValue = SPGraphics.QuickLabel(
+            self, fixed_height=21, align=Qt.AlignRight
+        )
+        self.labelTotalValue.setWordWrap(False)
+
+        self.labelMaxAmount = SPGraphics.QuickLabel(
+            self, fixed_height=21
+        )
+        self.labelMaxAmount.setWordWrap(False)
+        self.labelMaxAmount.setObjectName('labelDescription')
+
+        self.labelMaxAmountValue = SPGraphics.QuickLabel(
+            self, fixed_height=21, align=Qt.AlignRight
+        )
+        self.labelMaxAmountValue.setWordWrap(False)
+        self.labelMaxAmountValue.setObjectName('labelDescription')
+
+        self.layout().addWidget(self.labelEstimatedFee, 0, 0, 1, 1)
+        self.layout().addWidget(self.labelEstimatedFeeValue, 0, 1, 1, 1)
+        self.layout().addWidget(self.labelMaxFee, 1, 0, 1, 1)
+        self.layout().addWidget(self.labelMaxFeeValue, 1, 1, 1, 1)
+        self.layout().addWidget(self.labelTotal, 2, 0, 1, 1)
+        self.layout().addWidget(self.labelTotalValue, 2, 1, 1, 1)
+        self.layout().addWidget(self.labelMaxAmount, 3, 0, 1, 1)
+        self.layout().addWidget(self.labelMaxAmountValue, 3, 1, 1, 1)
+
+
 class UiForm(QWidget, SetupForm):
     def __init__(self, parent):
         super(UiForm, self).__init__(parent, flags=Qt.SubWindow)
 
         self.__pushButtonBack = None
         self.__networkWidget = None
-        self.__labelNoteIcon = None
-        self.__labelNetwork = None
-        self.__pushButtonNetwork = None
         self.__lineWidget = None
-        self.__labelToAddress = None
+        self.__labelAddress = None
         self.__labelFunctionName = None
-        self.__labelTokenIcon = None
-        self.__labelTokenValue = None
+        self.__labelIcon = None
+        self.__labelAmount = None
         self.__lineWidget2 = None
         self.__gasWidget = None
-        self.__labelEstimatedFee = None
-        self.__labelEstimatedFeeValue = None
-        self.__labelMaxFee = None
-        self.__labelMaxFeeValue = None
-        self.__labelTotal = None
-        self.__labelTotalValue = None
-        self.__labelMaxAmount = None
-        self.__labelMaxAmountValue = None
         self.__pushButtonConfirm = None
         self.__loadingEffectConfirm = None
 
@@ -45,28 +127,8 @@ class UiForm(QWidget, SetupForm):
         self.__pushButtonBack.move(10, 10)
         self.__pushButtonBack.clicked.connect(self.back_clicked)
 
-        self.__networkWidget = QWidget(self, flags=Qt.SubWindow)
-        self.__networkWidget.setSizePolicy(QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed))
-        self.__networkWidget.setLayout(QGridLayout())
-        self.__networkWidget.layout().setAlignment(Qt.AlignCenter)
-        self.__networkWidget.layout().setContentsMargins(0, 0, 0, 0)
-        self.__networkWidget.layout().setHorizontalSpacing(11)
-        self.__networkWidget.layout().setVerticalSpacing(0)
-
-        self.__labelNoteIcon = SPGraphics.QuickLabel(
-            self, fixed_size=Size.s41, pixmap=images.data.icons.info41, align=Qt.AlignCenter
-        )
-
-        self.__labelNetwork = SPGraphics.QuickLabel(
-            self, fixed_height=21
-        )
-
-        self.__pushButtonNetwork = SPGraphics.QuickPushButton(
-            fixed_height=21, value_changed=QObject.mainModel.textColorAnimated,
-            start_value=styles.data.colors.font_description, end_value=styles.data.colors.highlight
-        )
-        self.__pushButtonNetwork.setSizePolicy(QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed))
-        self.__pushButtonNetwork.clicked.connect(self.network_clicked)
+        self.__networkWidget = NetworkWidget(self)
+        self.__networkWidget.pushButton.clicked.connect(self.network_clicked)
 
         self.__lineWidget = SPGraphics.QuickWidget(
             self, fixed_height=1
@@ -74,21 +136,21 @@ class UiForm(QWidget, SetupForm):
         self.__lineWidget.setAttribute(Qt.WA_StyledBackground, True)
         self.__lineWidget.setObjectName('lineWidget')
 
-        self.__labelToAddress = qlabeladdress.QLabelAddress(
+        self.__labelAddress = qlabeladdress.QLabelAddress(
             self, tooltip=QObject.toolTip.transactionSendTo, copy_tooltip=QObject.toolTip.copyR
         )
-        self.__labelToAddress.setSizePolicy(QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed))
-        self.__labelToAddress.setCursor(Qt.PointingHandCursor)
+        self.__labelAddress.setSizePolicy(QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed))
+        self.__labelAddress.setCursor(Qt.PointingHandCursor)
 
         self.__labelFunctionName = SPGraphics.QuickLabel(
             self, fixed_height=21
         )
 
-        self.__labelTokenIcon = SPGraphics.QuickLabel(
+        self.__labelIcon = SPGraphics.QuickLabel(
             self, scaled=True, fixed_size=Size.s41, align=Qt.AlignCenter
         )
 
-        self.__labelTokenValue = SPGraphics.QuickLabel(
+        self.__labelAmount = SPGraphics.QuickLabel(
             self, fixed_height=41
         )
 
@@ -98,56 +160,7 @@ class UiForm(QWidget, SetupForm):
         self.__lineWidget2.setAttribute(Qt.WA_StyledBackground, True)
         self.__lineWidget2.setObjectName('lineWidget')
 
-        self.__gasWidget = QWidget(self, flags=Qt.SubWindow)
-        self.__gasWidget.setSizePolicy(QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed))
-        self.__gasWidget.setLayout(QGridLayout())
-        self.__gasWidget.layout().setContentsMargins(21, 21, 21, 21)
-        self.__gasWidget.layout().setSpacing(11)
-        self.__gasWidget.setObjectName('gasWidget')
-
-        self.__labelEstimatedFee = SPGraphics.QuickLabel(
-            self, fixed_height=21
-        )
-        self.__labelEstimatedFee.setWordWrap(False)
-
-        self.__labelEstimatedFeeValue = SPGraphics.QuickLabel(
-            self, fixed_height=21, align=Qt.AlignRight
-        )
-        self.__labelEstimatedFeeValue.setWordWrap(False)
-
-        self.__labelMaxFee = SPGraphics.QuickLabel(
-            self, fixed_height=21
-        )
-        self.__labelMaxFee.setWordWrap(False)
-        self.__labelMaxFee.setObjectName('labelDescription')
-
-        self.__labelMaxFeeValue = SPGraphics.QuickLabel(
-            self, fixed_height=21, align=Qt.AlignRight
-        )
-        self.__labelMaxFeeValue.setWordWrap(False)
-        self.__labelMaxFeeValue.setObjectName('labelDescription')
-
-        self.__labelTotal = SPGraphics.QuickLabel(
-            self, fixed_height=21
-        )
-        self.__labelTotal.setWordWrap(False)
-
-        self.__labelTotalValue = SPGraphics.QuickLabel(
-            self, fixed_height=21, align=Qt.AlignRight
-        )
-        self.__labelTotalValue.setWordWrap(False)
-
-        self.__labelMaxAmount = SPGraphics.QuickLabel(
-            self, fixed_height=21
-        )
-        self.__labelMaxAmount.setWordWrap(False)
-        self.__labelMaxAmount.setObjectName('labelDescription')
-
-        self.__labelMaxAmountValue = SPGraphics.QuickLabel(
-            self, fixed_height=21, align=Qt.AlignRight
-        )
-        self.__labelMaxAmountValue.setWordWrap(False)
-        self.__labelMaxAmountValue.setObjectName('labelDescription')
+        self.__gasWidget = GasWidget(self)
 
         self.__pushButtonConfirm = SPGraphics.QuickPushButton(
             self, fixed_size=Size.default, value_changed=QObject.mainModel.backgroundColorAnimated,
@@ -165,24 +178,13 @@ class UiForm(QWidget, SetupForm):
 
         self.layout().addWidget(self.__networkWidget, 0, 0, 1, 2)
         self.layout().addWidget(self.__lineWidget, 1, 0, 1, 2)
-        self.layout().addWidget(self.__labelToAddress, 2, 0, 1, 2, Qt.AlignHCenter)
+        self.layout().addWidget(self.__labelAddress, 2, 0, 1, 2, Qt.AlignHCenter)
         self.layout().addWidget(self.__labelFunctionName, 3, 0, 1, 2)
-        self.layout().addWidget(self.__labelTokenIcon, 4, 0, 1, 1)
-        self.layout().addWidget(self.__labelTokenValue, 4, 1, 1, 1, Qt.AlignLeft)
+        self.layout().addWidget(self.__labelIcon, 4, 0, 1, 1)
+        self.layout().addWidget(self.__labelAmount, 4, 1, 1, 1, Qt.AlignLeft)
         self.layout().addWidget(self.__lineWidget2, 5, 0, 1, 2)
         self.layout().addWidget(self.__gasWidget, 6, 0, 1, 2)
         self.layout().addWidget(self.__pushButtonConfirm, 7, 0, 1, 2, Qt.AlignHCenter)
-        self.__networkWidget.layout().addWidget(self.__labelNoteIcon, 0, 0, 2, 1)
-        self.__networkWidget.layout().addWidget(self.__labelNetwork, 0, 1, 1, 1)
-        self.__networkWidget.layout().addWidget(self.__pushButtonNetwork, 1, 1, 1, 1)
-        self.__gasWidget.layout().addWidget(self.__labelEstimatedFee, 0, 0, 1, 1)
-        self.__gasWidget.layout().addWidget(self.__labelEstimatedFeeValue, 0, 1, 1, 1)
-        self.__gasWidget.layout().addWidget(self.__labelMaxFee, 1, 0, 1, 1)
-        self.__gasWidget.layout().addWidget(self.__labelMaxFeeValue, 1, 1, 1, 1)
-        self.__gasWidget.layout().addWidget(self.__labelTotal, 2, 0, 1, 1)
-        self.__gasWidget.layout().addWidget(self.__labelTotalValue, 2, 1, 1, 1)
-        self.__gasWidget.layout().addWidget(self.__labelMaxAmount, 3, 0, 1, 1)
-        self.__gasWidget.layout().addWidget(self.__labelMaxAmountValue, 3, 1, 1, 1)
         self.__pushButtonConfirm.layout().addWidget(self.__loadingEffectConfirm, alignment=Qt.AlignCenter)
 
         super(UiForm, self).setup()
@@ -190,42 +192,42 @@ class UiForm(QWidget, SetupForm):
     def re_style(self):
         self.setStyleSheet(styles.data.css.transactionsender)
         self.__pushButtonBack.setIcon(QIcon(images.data.icons.changeable.arrow_left21))
-        self.__labelToAddress.setIcon(QIcon(images.data.icons.changeable.copy21))
+        self.__labelAddress.setIcon(QIcon(images.data.icons.changeable.copy21))
 
     def re_translate(self):
-        self.__labelNetwork.setText(translator("Current Network"))
-        self.__labelEstimatedFee.setText(translator("Estimated Gas Fee"))
-        self.__labelMaxFee.setText(translator("Max Fee"))
-        self.__labelTotal.setText(translator("Total"))
-        self.__labelMaxAmount.setText(translator("Max Amount"))
+        self.__networkWidget.labelTitle.setText(translator("Current Network"))
+        self.__gasWidget.labelEstimatedFee.setText(translator("Estimated Gas Fee"))
+        self.__gasWidget.labelMaxFee.setText(translator("Max Fee"))
+        self.__gasWidget.labelTotal.setText(translator("Total"))
+        self.__gasWidget.labelMaxAmount.setText(translator("Max Amount"))
         self.__pushButtonConfirm.setText(translator("Confirm"))
 
     def re_font(self):
         font = QFont()
 
-        self.__labelToAddress.setFont(font)
-        self.__labelMaxFee.setFont(font)
-        self.__labelMaxFeeValue.setFont(font)
-        self.__labelMaxAmount.setFont(font)
-        self.__labelMaxAmountValue.setFont(font)
+        self.__labelAddress.setFont(font)
+        self.__gasWidget.labelMaxFee.setFont(font)
+        self.__gasWidget.labelMaxFeeValue.setFont(font)
+        self.__gasWidget.labelMaxAmount.setFont(font)
+        self.__gasWidget.labelMaxAmountValue.setFont(font)
 
         font.setPointSize(fonts.data.size.title)
         font.setUnderline(True)
-        self.__pushButtonNetwork.setFont(font)
+        self.__networkWidget.pushButton.setFont(font)
 
         font.setUnderline(False)
-        self.__labelEstimatedFeeValue.setFont(font)
-        self.__labelTotalValue.setFont(font)
+        self.__gasWidget.labelEstimatedFeeValue.setFont(font)
+        self.__gasWidget.labelTotalValue.setFont(font)
 
         font.setBold(True)
-        self.__labelNetwork.setFont(font)
-        self.__labelEstimatedFee.setFont(font)
-        self.__labelTotal.setFont(font)
+        self.__networkWidget.labelTitle.setFont(font)
+        self.__gasWidget.labelEstimatedFee.setFont(font)
+        self.__gasWidget.labelTotal.setFont(font)
         self.__pushButtonConfirm.setFont(font)
 
         font.setPointSize(fonts.data.size.medium)
         self.__labelFunctionName.setFont(font)
-        self.__labelTokenValue.setFont(font)
+        self.__labelAmount.setFont(font)
 
     @pyqtSlot()
     def back_clicked(self):
@@ -243,40 +245,43 @@ class UiForm(QWidget, SetupForm):
     def confirm_clicked(self):
         self.__loadingEffectConfirm.start()
         self.__pushButtonBack.hide()
-        self.__pushButtonNetwork.setDisabled(True)
+        self.__networkWidget.pushButton.setDisabled(True)
         self.__pushButtonConfirm.setText("")
 
     def confirm_completed(self):
         self.__loadingEffectConfirm.stop()
         self.__pushButtonBack.show()
-        self.__pushButtonNetwork.setEnabled(True)
+        self.__networkWidget.pushButton.setEnabled(True)
         QTimer().singleShot(1000, self.re_translate)
 
     def set_data(
             self, network: str, address: str, function: str, amount: str, symbol: str,
             estimated_gas: str, max_fee: str, total: str, max_amount: str
     ):
-        self.__pushButtonNetwork.setText(network)
-        self.__labelToAddress.setText(address, False)
+        self.__networkWidget.pushButton.setText(network)
+        self.__labelAddress.setText(address, False)
         self.__labelFunctionName.setText(function)
-        self.__labelTokenIcon.setPixmap(assetsicons.get_asset_icon(symbol))
-        self.__labelTokenValue.setText(f'{amount} {symbol}')
-        self.__labelEstimatedFeeValue.setText(estimated_gas)
-        self.__labelMaxFeeValue.setText(f'{max_fee} {symbol}')
-        self.__labelTotalValue.setText(total)
-        self.__labelMaxAmountValue.setText(f'{max_amount} {symbol}')
+        self.__labelIcon.setPixmap(assetsicons.get_asset_icon(symbol))
+        self.__labelAmount.setText("{} {}".format(amount, symbol))
+        self.update_gas(estimated_gas, max_fee, total, max_amount, symbol)
+
+    def update_gas(self, estimated_gas: str, max_fee: str, total: str, max_amount: str, symbol: str):
+        self.__gasWidget.labelEstimatedFeeValue.setText(estimated_gas)
+        self.__gasWidget.labelMaxFeeValue.setText("{} {}".format(max_fee, symbol))
+        self.__gasWidget.labelTotalValue.setText(total)
+        self.__gasWidget.labelMaxAmountValue.setText("{} {}".format(max_amount, symbol))
         self.__refresh_effect()
 
     def reset(self):
-        self.__pushButtonNetwork.setText("")
-        self.__labelToAddress.clear()
+        self.__networkWidget.pushButton.setText("")
+        self.__labelAddress.clear()
         self.__labelFunctionName.clear()
-        self.__labelTokenIcon.clear()
-        self.__labelTokenValue.clear()
-        self.__labelEstimatedFeeValue.clear()
-        self.__labelMaxFeeValue.clear()
-        self.__labelTotalValue.clear()
-        self.__labelMaxAmountValue.clear()
+        self.__labelIcon.clear()
+        self.__labelAmount.clear()
+        self.__gasWidget.labelEstimatedFeeValue.clear()
+        self.__gasWidget.labelMaxFeeValue.clear()
+        self.__gasWidget.labelTotalValue.clear()
+        self.__gasWidget.labelMaxAmountValue.clear()
 
     def __refresh_effect(self):
         self.__pushButtonConfirm.setDisabled(True)
